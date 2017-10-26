ef1ad0589c1a3cf9ffbdc4034e89e9a674572ec7
bamboozooled <owoturotimi@live.co.uk>
2017-10-26 12:57:36 -0400

    first commit

---
+++ binarySearch.py
@@ -0,0 +1,21 @@
+def binarysearch(key,list1,begin,end,mid):
+    '''Condition for binary search: List must be sorted'''
+    mid = (begin+end)//2
+    list1=sorted(l)
+
+    if begin > end:
+        print("Nothing Found")
+        return None
+    if (begin == end and key == list1[mid]):
+        print(begin)
+    if (key == list1[mid]):
+        print(mid)
+    elif (key < list1[mid]):
+        end = mid-1
+        mid = (begin+end)/2
+        binarysearch(key,list1,begin,end,mid)
+    elif (key > list1[mid]):
+        begin = mid+1
+        mid = (begin+end)/2
+        binarysearch(key,list1,begin,end,mid)
+    
